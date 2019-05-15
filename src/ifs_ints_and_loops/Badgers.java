0f3658f4f5a9526619618c8ceebf0a0a700d9411
League Student <league@iMac-18.attlocal.net>
2019-05-14 18:28:21 -0700

    EEË :apple:

--- src/ifs_ints_and_loops/Badgers.java
+++ src/ifs_ints_and_loops/Badgers.java
@@ -1,5 +1,24 @@
 package ifs_ints_and_loops;
 
-public class Badgers {
+import javax.swing.JOptionPane;
 
+public class Badgers {
+public static void main(String[] args) {
+	for (int i = 0; i < 1.; i++) {
+		JOptionPane.showMessageDialog(null, "Badgers");
+		JOptionPane.showMessageDialog(null, "Badgers");
+		JOptionPane.showMessageDialog(null, "Badgers");
+		JOptionPane.showMessageDialog(null, "Badgers");
+		JOptionPane.showMessageDialog(null, "Badgers");
+		JOptionPane.showMessageDialog(null, "Badgers");
+		JOptionPane.showMessageDialog(null, "Badgers");
+		JOptionPane.showMessageDialog(null, "Badgers");
+		JOptionPane.showMessageDialog(null, "Badgers");
+		JOptionPane.showMessageDialog(null, "Badgers");
+		JOptionPane.showMessageDialog(null, "Badgers");
+		JOptionPane.showMessageDialog(null, "Badgers");
+		JOptionPane.showMessageDialog(null, "Mushroom");
+		JOptionPane.showMessageDialog(null, "Mushroom");
+	}
+}
 }
